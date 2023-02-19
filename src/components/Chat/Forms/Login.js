3b6963bbd95f8c6c03c581cfbe3bf5f49a57c3af
sh1nn0on3 <sanogaku31@gmail.com>
2023-02-19 13:19:13 +0700

    add

--- src/components/Chat/Forms/Login.js
+++ src/components/Chat/Forms/Login.js
@@ -11,9 +11,26 @@ export default function Login() {
           content="Ứng dụng nhắn tin với giao diện được thiết kế theo phong cách tối giản"
         />
       </Helmet>
-      <form>
-        <div></div>
+      <form className="min-h-screen flex flex-col gap-3 items-center justify-center ">
+        <h2 className="">Đăng Nhập vào ChatApp</h2>
+        <ul className="flex ">
+          <li className="p-2 border border-red-500 rounded-full">
+            <img
+              alt=""
+              src="https://www.xuanphuc.space/static/media/google.cadee8cdb0c0afb1a713.png"
+              className="w-12 "
+            />
+          </li>
+          <li className="p-2 border border-red-500 rounded-full">
+            <img
+              alt=""
+              src="https://www.xuanphuc.space/static/media/facebook.01a470d813915c298403.png"
+              className="w-12"
+            />
+          </li>
+        </ul>
+        <p></p>
       </form>
-    </> 
+    </>
   );
 }
